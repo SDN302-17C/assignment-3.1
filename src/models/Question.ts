afa905c2033de4d0ea76a883feb474e9bd54fdd0
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-18 21:46:33 +0700

    add: user model & verify user

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,16 +1,19 @@
 import mongoose, { Schema, Document, Types } from 'mongoose';
+
 interface IQuestion extends Document<Types.ObjectId> {
     text: string;
     options: string[];
     keywords: string[];
     correctAnswerIndex: number;
+    author: Types.ObjectId; 
 }
 
 const QuestionSchema: Schema = new Schema({
     text: { type: String, required: true },
     options: { type: [String], required: true },
-    keywords: { type: [String], required: true},
-    correctAnswerIndex: { type: Number, required: true }
+    keywords: { type: [String], required: true },
+    correctAnswerIndex: { type: Number, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true } 
 });
 
 export const Question = mongoose.model<IQuestion>('Question', QuestionSchema);
--- src/routes/QuizRoutes.ts
+++ src/routes/QuizRoutes.ts
@@ -9,16 +9,17 @@ import {
     addQuestionToQuiz,
     addQuestionsToQuiz
 } from '../controllers/QuizController';
+import { verifyUser, verifyAdmin } from '../controllers/Authenticate';
 
 const router = express.Router();
 
 router.get('/', getAllQuizzes);
 router.get('/:quizId', getQuizById);
-router.post('/', createQuiz);
-router.put('/:quizId', updateQuiz);
-router.delete('/:quizId', deleteQuiz);
+router.post('/', verifyUser, verifyAdmin, createQuiz);
+router.put('/:quizId', verifyUser, verifyAdmin, updateQuiz);
+router.delete('/:quizId', verifyUser, verifyAdmin, deleteQuiz);
 router.get('/:quizId/populate', getQuizByKeyword);
-router.post('/:quizId/question', addQuestionToQuiz);
-router.post('/:quizId/questions', addQuestionsToQuiz);
+router.post('/:quizId/question', verifyUser, verifyAdmin, addQuestionToQuiz);
+router.post('/:quizId/questions', verifyUser, verifyAdmin, addQuestionsToQuiz);
 
 export default router;
