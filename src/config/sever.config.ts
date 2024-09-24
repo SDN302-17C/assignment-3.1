ddb5ca10649652c837e12eb93562620424b64cd0
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-24 17:33:32 +0700

    add: origin,  jwtSecret & jwtExpiration to sever

--- src/config/sever.config.ts
+++ src/config/sever.config.ts
@@ -1,21 +1,30 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const DEVELOPMENT: boolean = process.env['APP_ENV'] == 'development';
-export const SCHEMA: string = process.env['SCHEMA'] || 'http';
-export const SERVER_HOST: string = process.env['SERVER_HOST'] || 'localhost';
+export const DEVELOPMENT: boolean = process.env["APP_ENV"] == "development";
+export const SCHEMA: string = process.env["SCHEMA"] || "http";
+export const SERVER_HOST: string = process.env["SERVER_HOST"] || "localhost";
 export const SERVER_PORT: number = parseInt(
-	process.env['SERVER_PORT'] || '3000',
+  process.env["SERVER_PORT"] || "3000"
 );
+export const SERVER_ORIGIN: string = DEVELOPMENT
+	? '*'
+	: (process.env['ORIGIN'] as string) || 'http://localhost:3000';
 export const SERVER_URL: string = `${SCHEMA}://${SERVER_HOST}:${SERVER_PORT}`;
 export const DATABASE_URL: string =
-	process.env['DATABASE_URL'] || 'mongodb://localhost:27017/SimpleQuiz';
+  process.env["DATABASE_URL"] || "mongodb://localhost:27017/SimpleQuiz";
+export const JWT_SECRET: string = process.env["JWT_SECRET"] || "secret";
+
+export const JWT_EXPIRATION: string = process.env["JWT_EXPIRATION"] || "1h";
 
 export const server = {
-	schema: SCHEMA,
-	host: SERVER_HOST,
-	port: SERVER_PORT,
-	serverUrl: SERVER_URL,
-	databaseUrl: DATABASE_URL,
+  schema: SCHEMA,
+  host: SERVER_HOST,
+  port: SERVER_PORT,
+  origin: SERVER_ORIGIN,
+  serverUrl: SERVER_URL,
+  databaseUrl: DATABASE_URL,
+  jwtSecret: JWT_SECRET,
+  jwtExpiration: JWT_EXPIRATION,
 };
