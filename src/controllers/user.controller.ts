ef900571df172d951dd9d4d5de42c7b56c5c3721
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-27 23:07:13 +0700

    update: crud api for user & fix check login for auth

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { User } from "../models/user.model";
-import { handleErrors } from "../services/utils.service";
+import { IUser, User } from "../models/user.model";
+import { handleErrors, hashPassword } from "../services/utils.service";
 
 export const getAllUsers = async (
   req: Request,
@@ -16,10 +16,10 @@ export const getAllUsers = async (
 
 export const getUserById = async (
   res: Response,
-  userId: string
+  req: Request
 ): Promise<void> => {
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(req.params["userId"]);
     if (!user) {
       res.status(404).json({ error: "User not found" });
       return;
@@ -30,16 +30,84 @@ export const getUserById = async (
   }
 };
 
-export const getUserByUsername = async (username: string) => {
-	try {
-		const user = await User.findOne({ username: username });
+export const createUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { username, password, admin } = req.body;
 
-		if (!user) {
-			throw new Error('User not found');
-		}
+    if (!username || !password) {
+      res.status(400).json({ error: "Username and password are required" });
+      return;
+    }
 
-		return user;
-	} catch (error) {
-		throw new Error('Error fetching user from database');
-	}
+    const userExists = await User.findOne({ username: username });
+    if (userExists) {
+      res.status(400).json({ error: "User already exists" });
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User(
+      <IUser>{
+        username: username,
+        hashedPassword: hashedPassword,
+        admin: admin || false,
+      }
+    );
+
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    handleErrors(res, error);
+  }
+}
+
+export const updateUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { username, password, admin } = req.body;
+
+    if (!username || !password) {
+      res.status(400).json({ error: "Username and password are required" });
+      return;
+    }
+
+    const user = await User.findById(req.params["userId"]);
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    user.username = username;
+    user.hashedPassword = await hashPassword(password);
+    user.admin = admin || false;
+
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    handleErrors(res, error);
+  }
+};
+
+export const deleteUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const user = await User.findById(req.params["userId"]);
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    await user.deleteOne();
+    res.json(user);
+  } catch (error) {
+    handleErrors(res, error);
+  }
 };
