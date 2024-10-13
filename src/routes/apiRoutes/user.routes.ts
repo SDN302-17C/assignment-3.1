3ca7297be640ae564d82067f582a48fcd89a0e3f
Quốc Chương <quocchuong2609@gmail.com>
2024-10-13 22:04:41 +0700

    delete: api get user by username

--- src/routes/apiRoutes/user.routes.ts
+++ src/routes/apiRoutes/user.routes.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { createUser, deleteUser, getAllUsers, getUserById, getUserByUsername, updateUser } from "../../controllers/user.controller";
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../../controllers/user.controller";
 import { verifyAdmin } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/", verifyAdmin, getAllUsers);
-router.get("/:userId",  verifyAdmin, getUserById);
-router.get("/:username",  getUserByUsername);
+router.get("/:userId", getUserById);
 router.post("/",  verifyAdmin,createUser);
 router.put("/:userId", verifyAdmin, updateUser);
 router.delete("/:userId",  verifyAdmin,deleteUser);
